fb7fa5e8c8b69274f85bcabfdc75e6ba12de7d9e
Howard Henson <howard@henson.me.uk>
2023-10-31 20:06:13 +0400

    Prepare test for recorder

---
+++ test/python/unit/hg/nodes/test_recorder.py
@@ -0,0 +1,22 @@
+from hg import graph, TS, run_graph, MIN_ST, MIN_TD
+from hg.nodes import set_replay_values, SimpleArrayReplaySource, replay, record, get_recorded_value
+
+
+def test_recorder():
+    """
+    Note, this test depends on the replay functionality to be valid.
+    So bootstrap replay before validating the recorder.
+    Once this is working the unit tester should become functional.
+    """
+
+    set_replay_values("test", SimpleArrayReplaySource(["1", "2", "3"]))
+
+    @graph
+    def main():
+        value_ts = replay("test", TS[str])
+        record(value_ts)
+
+    run_graph(main)
+
+    values = get_recorded_value()
+    assert values == [(MIN_ST, "1"), (MIN_ST+MIN_TD, "2"), (MIN_ST+2*MIN_TD, "3")]
