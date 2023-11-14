417d5bd0e870cd869c4a60f83f50a2e7a7016148
Howard Henson <howard@henson.me.uk>
2023-11-14 22:01:10 +0400

    Initial structure for TSD

---
+++ hg/_impl/_types/_tsd.py
@@ -0,0 +1,155 @@
+from typing import Generic, Any, Iterable, Tuple
+
+from frozendict import frozendict
+
+from hg import TimeSeriesOutput, PythonTimeSeriesBuilderFactory, TSOutputBuilder, TSInputBuilder
+from hg._impl._types._input import PythonBoundTimeSeriesInput
+from hg._impl._types._output import PythonTimeSeriesOutput
+from hg._types._time_series_types import K, V
+from hg._types._tsd_type import TimeSeriesDictOutput, TimeSeriesDictInput, REMOVE_KEY_IF_EXISTS, REMOVE_KEY
+
+
+class TSDKeyObserver:
+    """
+    Used to track additions and removals of parent keys.
+    Since the TSD is dynamic, the inputs associated to an output needs to be updated when a key is added or removed.
+    in order to correctly manage it's internal state.
+    """
+
+    def on_key_added(self, key: K):
+        pass
+
+    def on_key_removed(self, key: K):
+        pass
+
+
+class PythonTimeSeriesDictOutput(PythonTimeSeriesOutput, TimeSeriesDictOutput[K, V], Generic[K, V]):
+
+    def __init__(self, __key_tp__, __value_tp__, *args, **kwargs):
+        Generic.__init__(self)
+        TimeSeriesDictOutput.__init__(self, __key_tp__, __value_tp__)
+        super().__init__(*args, **kwargs)
+        self._key_observers: list[TSDKeyObserver] = []
+        self._ts_builder: TSOutputBuilder = PythonTimeSeriesBuilderFactory.instance().make_output_builder(__value_tp__)
+        self._removed_items: dict[K, V] = {}
+        self._added_keys: set[str] = set()
+
+    def add_key_observer(self, observer: TSDKeyObserver):
+        self._key_observers.append(observer)
+
+    def remove_key_observer(self, observer: TSDKeyObserver):
+        self._key_observers.remove(observer)
+
+    @property
+    def value(self) -> frozendict:
+        return frozendict({k: v.value for k, v in self.items() if v.valid})
+
+    @property
+    def delta_value(self):
+        return frozendict({k: v.delta_value for k, v in self.items() if v.modified})
+
+    def apply_result(self, result: Any):
+        if result is None:
+            return
+        # Expect a mapping of some sort or an iterable of k, v pairs
+        for k, v in result.items() if isinstance(result, (dict, frozendict)) else result:
+            if v in (REMOVE_KEY, REMOVE_KEY_IF_EXISTS):
+                if k not in self._removed_items:
+                    if v is REMOVE_KEY:
+                        raise KeyError(f"TSD[{self.__key_tp__}, {self.__value_tp__}] Key {k} does not exist")
+                    else:
+                        continue
+                self._removed_items[k] = self._ts_values.pop(k)
+                for observer in self._key_observers:
+                    observer.on_key_removed(k)
+                continue
+            elif k not in self._ts_values:
+                self._ts_values[k] = self._ts_builder.make_instance(owning_output=self)
+                for observer in self._key_observers:
+                    observer.on_key_added(k)
+            self[k].apply_result(v)
+        if self._removed_items or self._added_keys:
+            self.owning_graph.context.add_after_evaluation_notification(self._clear_key_changes)
+
+    def _clear_key_changes(self):
+        self._removed_items = {}
+        self._added_keys = set()
+
+    def copy_from_output(self, output: "TimeSeriesOutput"):
+        output: PythonTimeSeriesDictOutput
+        for k, v in output.items():
+            self[k].copy_from_output(v)
+
+    def copy_from_input(self, input: "TimeSeriesInput"):
+        input: PythonTimeSeriesDictInput
+        for k, v in input.items():
+            self[k].copy_from_input(v)
+
+    def added_keys(self) -> Iterable[K]:
+        return self._added_keys
+
+    def added_values(self) -> Iterable[V]:
+        return (self[k] for k in self._added_keys)
+
+    def added_items(self) -> Iterable[Tuple[K, V]]:
+        return (k, self[k] for k in self._added_keys)
+
+    def removed_keys(self) -> Iterable[K]:
+        return self._removed_items.keys()
+
+    def removed_values(self) -> Iterable[V]:
+        return self._removed_items.values()
+
+    def removed_items(self) -> Iterable[Tuple[K, V]]:
+        return self._removed_items.items()
+
+
+class PythonTimeSeriesDictInput(PythonBoundTimeSeriesInput, TimeSeriesDictInput[K, V], TSDKeyObserver, Generic[K, V]):
+
+    # At the moment the only supported inptut type is a bound input. When we start to support time-series references
+    # this will need to change
+
+    def __init__(self, __key_tp__, __value_tp__, *args, **kwargs):
+        Generic.__init__(self)
+        TimeSeriesDictInput.__init__(self, __key_tp__, __value_tp__)
+        PythonBoundTimeSeriesInput.__init__(self, *args, **kwargs)
+        self._ts_builder: TSInputBuilder = PythonTimeSeriesBuilderFactory.instance().make_input_builder(__value_tp__)
+        self._removed_items: dict[K, V] = {}
+
+    def bind_output(self, output: TimeSeriesOutput):
+        super().bind_output(output)
+        output: PythonTimeSeriesDictOutput
+        output.add_key_observer(self)
+
+    def on_key_added(self, key: K):
+        self._ts_values[key] = (v := self._ts_builder.make_instance(owning_input=self))
+        v.bind_output(self.output[key])
+
+    def on_key_removed(self, key: K):
+        if not self._removed_items:
+            self.owning_graph.context.add_after_evaluation_notification(self._clear_key_changes)
+        self._removed_items[key] = self._ts_values.pop(key)
+
+    def _clear_key_changes(self):
+        self._removed_items = {}
+
+    def added_keys(self) -> Iterable[K]:
+        output: PythonTimeSeriesDictOutput = self.output
+        return output.added_keys()
+
+    def added_values(self) -> Iterable[V]:
+        return (self[k] for k in self.added_keys())
+
+    def added_items(self) -> Iterable[Tuple[K, V]]:
+        return ((k, self[k]) for k in self.added_keys())
+
+    def removed_keys(self) -> Iterable[K]:
+        return self._removed_items.keys()
+
+    def removed_values(self) -> Iterable[V]:
+        return self._removed_items.values()
+
+    def removed_items(self) -> Iterable[Tuple[K, V]]:
+        return self._removed_items.items()
+
+
--- hg/_types/_tsd_type.py
+++ hg/_types/_tsd_type.py
@@ -1,13 +1,19 @@
-from abc import abstractmethod
-from typing import Generic, Iterable
+from abc import abstractmethod, ABC
+from typing import Generic, Iterable, Mapping, Union, Any, TYPE_CHECKING, Tuple
 
 from frozendict import frozendict
 
 from hg._types._time_series_types import TimeSeriesIterable, TimeSeriesInput, TimeSeriesOutput, K, V, \
     TimeSeriesDeltaValue
 
+if TYPE_CHECKING:
+    from hg import HgScalarTypeMetaData, HgTimeSeriesTypeMetaData
 
-__all__ = ("TSD", "TSD_OUT", "TimeSeriesDict", "TimeSeriesDictInput", "TimeSeriesDictOutput")
+__all__ = ("TSD", "TSD_OUT", "TimeSeriesDict", "TimeSeriesDictInput", "TimeSeriesDictOutput", "REMOVE_KEY", "REMOVE_KEY_IF_EXISTS")
+
+
+REMOVE_KEY = object()
+REMOVE_KEY_IF_EXISTS = object()
 
 
 class TimeSeriesDict(TimeSeriesIterable[K, V], TimeSeriesDeltaValue[frozendict, frozendict], Generic[K, V]):
@@ -15,32 +21,148 @@ class TimeSeriesDict(TimeSeriesIterable[K, V], TimeSeriesDeltaValue[frozendict, 
     A TSD is a collection of time-series values keyed off of a scalar key K.
     """
 
-    @abstractmethod
+    def __init__(self, __key_tp__: "HgScalarTypeMetaData", __value_tp__: "HgTimeSeriesTypeMetaData"):
+        Generic.__init__(self)
+        TimeSeriesDeltaValue.__init__(self)
+        TimeSeriesIterable.__init__(self)
+        self.__key_tp__: "HgScalarTypeMetaData" = __key_tp__
+        self.__value_tp__: "HgTimeSeriesTypeMetaData" = __value_tp__
+        self._ts_values: dict[str, Union[TimeSeriesInput, TimeSeriesOutput]] = {}
+
+    def __class_getitem__(cls, item) -> Any:
+        # For now limit to validation of item
+        out = super(TimeSeriesDict, cls).__class_getitem__(item)
+        if type(item) is not tuple or len(item) != 2:
+            return out
+        if item != (K, V):
+            from hg._types._type_meta_data import HgTypeMetaData
+            __key_tp__ = HgTypeMetaData.parse(item[0])
+            __value_tp__ = HgTypeMetaData.parse(item[1])
+            if not __key_tp__.is_scalar:
+                from hg import ParseError
+                raise ParseError(
+                    f"For TSD[{__key_tp__}][{__value_tp__}], '{__value_tp__}', '{__key_tp__}' must be a SCALAR type")
+            if __value_tp__.is_scalar:
+                from hg import ParseError
+                raise ParseError(
+                    f"For TSD[{__key_tp__}][{__value_tp__}], '{__value_tp__}' must be a TIME_SERIES_TYPE type")
+            out.__key_tp__ = __key_tp__
+            out.__value_tp__ = __value_tp__
+            _init = out.__init__
+            out.__init__ = lambda *args, **kwargs: _init(out.__key_tp__, out.__value_tp__, *args, **kwargs)
+        return out
+
     def __getitem__(self, item: K) -> V:
         """
         Returns the time series at this index position
         :param item:
         :return:
         """
+        return self._ts_values[item]
 
-    @abstractmethod
     def __iter__(self) -> Iterable[K]:
         """
         Iterator over the time-series values
         :return:
         """
+        return iter(self._ts_values)
+
+    def keys(self) -> Iterable[K]:
+        return self._ts_values.keys()
+
+    def values(self) -> Iterable[V]:
+        return self._ts_values.values()
+
+    def items(self) -> Iterable[Tuple[K, V]]:
+        return self._ts_values.items()
+
+    def modified_keys(self) -> Iterable[K]:
+        return (k for k in self.keys() if self._ts_values[k].modified)
+
+    def modified_values(self) -> Iterable[V]:
+        return (v for v in self.values() if v.modified)
+
+    def modified_items(self) -> Iterable[Tuple[K, V]]:
+        return ((k, v) for k, v in self.items() if v.modified)
+
+    def valid_keys(self) -> Iterable[K]:
+        return (k for k in self.keys() if self._ts_values[k].valid)
+
+    def valid_values(self) -> Iterable[V]:
+        return (v for v in self.values() if v.valid)
+
+    def valid_items(self) -> Iterable[Tuple[K, V]]:
+        return ((k, v) for k, v in self.items() if v.valid)
+
+    @abstractmethod
+    def added_keys(self) -> Iterable[K]:
+        """
+        Returns the keys that were added since the last tick.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def added_values(self) -> Iterable[V]:
+        """
+        Returns the values that were added since the last tick.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def added_items(self) -> Iterable[Tuple[K, V]]:
+        """
+        Returns the items that were added since the last tick.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def removed_keys(self) -> Iterable[K]:
+        """
+        Returns the keys that were removed since the last tick.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def removed_values(self) -> Iterable[V]:
+        """
+        Returns the values that were removed since the last tick.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def removed_items(self) -> Iterable[Tuple[K, V]]:
+        """
+        Returns the items that were removed since the last tick.
+        :return:
+        """
+        pass
 
 
-class TimeSeriesDictInput(TimeSeriesInput, TimeSeriesDict[K, V], Generic[K, V]):
+class TimeSeriesDictInput(TimeSeriesInput, TimeSeriesDict[K, V], ABC, Generic[K, V]):
     """
     The TSD input
     """
 
+    def __init__(self, __key_tp__, __value_tp__):
+        Generic.__init__(self)
+        TimeSeriesDict.__init__(self, __key_tp__, __value_tp__)
+        TimeSeriesInput.__init__(self)
 
-class TimeSeriesDictOutput(TimeSeriesOutput, TimeSeriesDict[K, V], Generic[K, V]):
+
+class TimeSeriesDictOutput(TimeSeriesOutput, TimeSeriesDict[K, V], ABC, Generic[K, V]):
     """
     The TSD output
     """
+
+    def __init__(self, __key_tp__, __value_tp__):
+        Generic.__init__(self)
+        TimeSeriesDict.__init__(self, __key_tp__, __value_tp__)
+        TimeSeriesOutput.__init__(self)
 
     value: frozendict
 
