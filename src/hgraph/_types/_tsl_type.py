fd3192db1e70f4ae89b530df27ab81f6e7fcba19
Howard Henson <howard@henson.me.uk>
2023-12-21 19:58:25 +0000

    Small cleanups as suggested by Sonar

--- src/hgraph/_types/_tsl_type.py
+++ src/hgraph/_types/_tsl_type.py
@@ -30,9 +30,8 @@ class TimeSeriesList(TimeSeriesIterable[int, TIME_SERIES_TYPE], TimeSeriesDeltaV
     def __class_getitem__(cls, item) -> Any:
         # For now limit to validation of item
         is_tuple = type(item) is tuple
-        if not is_tuple or len(item) != 2:
-            if is_tuple:
-                item = (item[0] if len(item) == 1 else TIME_SERIES_TYPE), SIZE
+        if is_tuple and len(item) != 2:
+            item = (item[0] if len(item) == 1 else TIME_SERIES_TYPE), SIZE
         out = super(TimeSeriesList, cls).__class_getitem__(item)
         if item != (TIME_SERIES_TYPE, SIZE):
             from hgraph._types._type_meta_data import HgTypeMetaData
