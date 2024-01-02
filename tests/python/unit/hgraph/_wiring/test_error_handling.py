91a28a54805d79ece155fdae1ff81e41f1826067
Howard Henson <howard@henson.me.uk>
2024-01-02 20:56:36 +0400

    Error handling for map_ dict working

--- tests/python/unit/hgraph/_wiring/test_error_handling.py
+++ tests/python/unit/hgraph/_wiring/test_error_handling.py
@@ -1,5 +1,8 @@
-from hgraph import graph, TS, TSB, NodeError, ts_schema
+from frozendict import frozendict
+
+from hgraph import graph, TS, TSB, NodeError, ts_schema, TSD, map_, REF
 from hgraph._wiring.error_context import error_context, error_time_series
+from hgraph.nodes import div_, debug_print
 from hgraph.test import eval_node
 
 
@@ -17,3 +20,18 @@ def test_error_handling():
     assert result[2].keys() == {"error"}
 
     print(result[2]["error"])
+
+
+def test_error_handling_with_map():
+    schema = ts_schema(out=TSD[int, REF[TS[float]]], error=TSD[int, TS[NodeError]])
+
+    @graph
+    def main(lhs: TSD[int, TS[float]], rhs: TSD[int, TS[float]]) -> TSB[schema]:
+        out = map_(div_, lhs, rhs)
+        return TSB[schema].from_ts(out=out, error=error_time_series(out))
+
+    result = eval_node(main, [{0: 1.0}, {1: 2.0}, {2: 3.0}], [{0: 1.0}, {1: 2.0}, {2: 0.0}])
+    assert result[0:2] == [{"out": frozendict({0: 1.0})}, {"out": frozendict({1: 1.0})}]
+    assert "error" in result[2].keys()
+
+   # print(result[2]["error"][2])
