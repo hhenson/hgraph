06aa87ff4316a0f1ba2df081e30d73eeb7132b32
Howard Henson <howard@henson.me.uk>
2024-01-05 21:30:02 +0400

    Add test for not_

--- src/hgraph/nodes/_logical.py
+++ src/hgraph/nodes/_logical.py
@@ -3,6 +3,7 @@ from hgraph import compute_node, TS
 
 __all__ = ("not_",)
 
+
 @compute_node
 def not_(ts: TS[bool]) -> TS[bool]:
     return not ts.value
---
+++ tests/python/unit/hgraph/nodes/test_logical.py
@@ -0,0 +1,6 @@
+from hgraph.nodes import not_
+from hgraph.test import eval_node
+
+
+def test_not():
+    assert eval_node(not_, [True, False]) == [False, True]
