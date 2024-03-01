d40123a6578b21987327cf38ad23fe2ce05f1f2e
Howard Henson <howard@henson.me.uk>
2024-03-01 20:52:49 +0400

    Update data writer

--- src/hgraph/_runtime/_data_writer.py
+++ src/hgraph/_runtime/_data_writer.py
@@ -16,21 +16,21 @@ class DataWriter(ABC):
         self.write(b)
 
     def write_date(self, d: date):
-        self.write((d.year * 10000 + d.month * 100 + d.day).to_bytes(32 // 8, byteorder='big'))
+        self.write((d.year * 10000 + d.month * 100 + d.day).to_bytes(32 // 8, byteorder='big', signed=True))
 
     def write_datetime(self, d: datetime):
-        self.write((int(d.timestamp()) * 1000000 + d.microsecond).to_bytes(64 // 8,
-                                                                           byteorder='big'))  # actually only needs 54 bits length
+        self.write_float(d.timestamp())
 
     def write_time_delta(self, td: timedelta):
-        self.write((int(td.total_seconds()) * 1000000 + td.microseconds).to_bytes(64 // 8, byteorder='big'))
+        self.write(
+            (int(td.total_seconds()) * 1000000 + td.microseconds).to_bytes(64 // 8, byteorder='big', signed=True))
 
     def write_float(self, f: float):
         import struct
         self.write(struct.pack('d', f))
 
     def write_int(self, i: int):
-        self.write(i.to_bytes(64 // 8, byteorder='big'))
+        self.write(i.to_bytes(64 // 8, byteorder='big', signed=True))
 
     def write_string(self, s: str):
         b = s.encode('utf-8')
@@ -43,7 +43,7 @@ class DataWriter(ABC):
     def write_time(self, tm: time):
         # only need 37 bits, but since it is more than 32 will pack into a 64 bit structure
         self.write((((tm.hour * 3600 + tm.minute * 60 + tm.second) * 1000000) +
-                    tm.microsecond).to_bytes(64 // 8, byteorder='big'))
+                    tm.microsecond).to_bytes(64 // 8, byteorder='big', signed=True))
 
 
 class DataReader(ABC):
@@ -57,17 +57,15 @@ class DataReader(ABC):
         return self.read(sz)
 
     def read_date(self) -> date:
-        i = int.from_bytes(self.read(32 // 8), "big")
+        i = int.from_bytes(self.read(32 // 8), "big", signed=True)
         year = i // 10000
         month = i // 100 - year * 10000
         day = i - year * 10000 - month * 100
         return date(year, month, day)
 
     def read_datetime(self) -> datetime:
-        i = self.read_int()
-        seconds = i // 1000000
-        microseconds = i - seconds
-        return datetime.fromtimestamp(seconds) + timedelta(microseconds=microseconds)
+        ts = self.read_float()
+        return datetime.fromtimestamp(ts)
 
     def read_time_delta(self) -> timedelta:
         i = self.read_int()
@@ -77,10 +75,10 @@ class DataReader(ABC):
 
     def read_float(self) -> float:
         import struct
-        return struct.unpack('d', self.read(64 // 8))
+        return struct.unpack('d', self.read(64 // 8))[0]
 
     def read_int(self) -> int:
-        return int.from_bytes(self.read(64 // 8), "big")
+        return int.from_bytes(self.read(64 // 8), "big", signed=True)
 
     def read_string(self) -> str:
         sz = self.read_int()
