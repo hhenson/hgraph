293a192f239a25b7024f2c7181e7be5d38ffbcc3
Howard Henson <howard@henson.me.uk>
2024-03-02 11:39:17 +0400

    Add support to return the result of a graph with an unbound output.

--- tests/python/unit/hgraph/_impl/_runtime/test_graph_runner.py
+++ tests/python/unit/hgraph/_impl/_runtime/test_graph_runner.py
@@ -1,4 +1,6 @@
-from hgraph import graph, run_graph, compute_node, TS, TIME_SERIES_TYPE
+from datetime import datetime
+
+from hgraph import graph, run_graph, compute_node, TS, TIME_SERIES_TYPE, evaluate_graph, GraphConfiguration
 from hgraph._runtime._evaluation_engine import EvaluationMode
 from hgraph.nodes import const, print_
 from hgraph.nodes._print import debug_print
@@ -27,3 +29,12 @@ def test_compute_node():
         debug_print("t", t)
 
     run_graph(hello_world, run_mode=EvaluationMode.SIMULATION)
+
+
+def test_return_result():
+
+    @graph
+    def hello_world() -> TS[int]:
+        return const(1)
+
+    assert evaluate_graph(hello_world, GraphConfiguration()) == [(datetime(1970, 1, 1, 0, 0, 0, 1), 1)]
