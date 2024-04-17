1693cac79809f87a3fa409b81bcef7e08f4d1db3
Howard Henson <howard@henson.me.uk>
2024-04-17 19:54:02 +0400

    Add cash instruments

--- src/hg_oap/instruments/cash.py
+++ src/hg_oap/instruments/cash.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+from enum import Enum
 
-from hg_oap.assets.currency import Currency
+from hg_oap.assets.currency import Currency, Currencies
 from hg_oap.instruments.instrument import Instrument
 
 
@@ -8,4 +9,11 @@ from hg_oap.instruments.instrument import Instrument
 class Cash(Instrument):
     currency: Currency
 
+
+class CashInstruments(Enum):
+    EUR = Cash("EUR", asset=Currencies.EUR)
+    GBP = Cash("GBP", asset=Currencies.GBP)
+    GBX = Cash("GBX", asset=Currencies.GBX)
+    USD = Cash("USD", asset=Currencies.USD)
+    USX = Cash("USX", asset=Currencies.USX)
 
