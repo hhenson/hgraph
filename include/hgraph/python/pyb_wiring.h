9d84ef00bb0a4c5db3fb1d643594075466edad83
Howard Henson <howard@henson.me.uk>
2024-05-04 20:37:05 +0400

    Initial structure for C++ code

---
+++ include/hgraph/python/pyb_wiring.h
@@ -0,0 +1,15 @@
+//
+// Created by Howard Henson on 26/02/2023.
+//
+
+#ifndef HGRAPH_PYB_WIRING_H
+#define HGRAPH_PYB_WIRING_H
+
+#include <hgraph/python/pyb.h>
+#include <pybind11/stl.h>
+#include <pybind11/chrono.h>
+
+using namespace py::literals;
+
+
+#endif  // HGRAPH_PYB_WIRING_H
---
+++ src/cpp/python/_hgraph_module.cpp
@@ -0,0 +1,20 @@
+/*
+ * The entry point into the python _hgraph module exposing the C++ types to python.
+ *
+ * Note that as a pattern, we will return alias shared pointers for objects that have their life-times managed by an outer object
+ * such as ExecutionGraph, where the life-time of the objects contained within are all directly managed by the outer object.
+ * This reduces the number of shared pointers that need to be constructed inside the graph and thus provides a small improvement
+ * on memory and general performance.
+ *
+ */
+
+#include <hgraph/python/pyb_wiring.h>
+
+void export_graph_elements(py::module&);
+
+
+PYBIND11_MODULE(_hgraph, m) {
+    m.doc() = "The HGraph C++ runtime engine";
+
+    export_graph_elements(m);
+}
