b43ab0eb69415338bf8dfeb33bb0f04798babfda
Howard Henson <howard@henson.me.uk>
2024-05-05 20:29:45 +0400

    Add Graph and Evaluation engine interface.

--- include/hgraph/types/graph.h
+++ include/hgraph/types/graph.h
@@ -9,17 +9,38 @@
 #include<optional>
 #include<vector>
 
+#include "hgraph/runtime/evaluation_engine.h"
+
 namespace hgraph {
-    class Node;
+    struct Node;
 
-    struct HGRAPH_EXPORT Graph: ComponentLifeCycle {
-        std::optional<Node*> parent_node;
+    struct HGRAPH_EXPORT Graph : ComponentLifeCycle {
+        Graph(std::vector<int64_t> graph_id_, std::vector<Node *> nodes_, std::optional<Node *> parent_node_,
+              std::optional<std::string> label_);
+
+        std::optional<Node *> parent_node;
         std::vector<int64_t> graph_id;
-        std::string label;
-        std::vector<Node*> nodes;
+        std::optional<std::string> label;
+        std::vector<Node *> &nodes;
 
+        [[nodiscard]] EvaluationEngineApi *evaluation_engine_api() const;
+
+        [[nodiscard]] EvaluationClock *evaluation_clock() const;
+
+        [[nodiscard]] EvaluationEngine *evaluation_engine() const;
+
+        void set_evaluation_engine(EvaluationEngine *value);
+
+        int64_t push_source_nodes_end();
+
+        void schedule_node(int64_t node_ndx, engine_time_t when);
+
+        std::vector<engine_time_t> &schedule();
+
+        void evaluation_graph();
+
+        std::unique_ptr<Graph> copy_with(std::vector<Node *> nodes);
     };
-
 }
 
 #endif //GRAPH_H
