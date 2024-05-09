040dcc19cfbe3ea66f43df35c1f4f3f97bc73969
Simon Young <syoung@bamfunds.com>
2024-05-09 15:32:05 +0100

    Add drop_dups float version.  Add signal.  Fix generator outputting timedeltas

--- src/hgraph/nodes/_drop_dups.py
+++ src/hgraph/nodes/_drop_dups.py
@@ -1,7 +1,8 @@
-from hgraph import compute_node, TIME_SERIES_TYPE
+import math
 
+from hgraph import compute_node, TIME_SERIES_TYPE, TS
 
-__all__ = ("drop_dups",)
+__all__ = ("drop_dups", "drop_dups_float")
 
 
 @compute_node
@@ -11,6 +12,18 @@ def drop_dups(ts: TIME_SERIES_TYPE, _output: TIME_SERIES_TYPE = None) -> TIME_SE
     """
     if _output.valid:
         if ts.value != _output.value:
+            return ts.delta_value
+    else:
+        return ts.value
+
+
+@compute_node(overloads=drop_dups)
+def drop_dups_float(ts: TS[float], abs_tol: float = 1e-15, _output: TS[float] = None) -> TS[float]:
+    """
+    Drops 'duplicate' float values from a time-series which are almost equal
+    """
+    if _output.valid:
+        if not (-abs_tol < ts.value - _output.value < abs_tol):
             return ts.delta_value
     else:
         return ts.value
