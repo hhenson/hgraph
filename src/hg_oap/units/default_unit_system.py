fa0c2f5999ee89118880800befea35e6bd678c67
Simon Young <syoung@bamfunds.com>
2024-05-23 15:57:46 +0100

    cleanup

--- src/hg_oap/units/default_unit_system.py
+++ src/hg_oap/units/default_unit_system.py
@@ -7,20 +7,20 @@ from hg_oap.units.unit_system import UnitSystem
 __all__ = ("U")
 
 
-std_prefixes = {'k': 1000.0, 'M': 1_000_000.0, 'G': 1_000_000_000.0, 'T': 1_000_000_000_000.0,
-                'c': 0.01, 'm': 0.001, 'u': 0.000_001, 'n': 0.000_000_001, 'p': 0.000_000_000_001}
+std_prefixes = {'k': Decimal('1000'), 'M': Decimal('1_000_000'), 'G': Decimal('1_000_000_000'), 'T': Decimal('1_000_000_000_000'),
+                'c': Decimal('0.01'), 'm': Decimal('0.001'), 'u': Decimal('0.000_001'), 'n': Decimal('0.000_000_001'), 'p': Decimal('0.000_000_000_001')}
 
 U = UnitSystem(__prefixes__=std_prefixes)
 U.register()  # If this has been imported we can go ahead and register it.
 
 U.length = PrimaryDimension()
 U.m = PrimaryUnit(dimension=U.length, prefixes=('k', 'c', 'm', 'u', 'n'))
-U.mi = DerivedUnit(primary_unit=U.m, ratio=1609.344)
-U.nautical_mile = DerivedUnit(primary_unit=U.m, ratio=1852.0)
+U.mi = DerivedUnit(primary_unit=U.m, ratio=Decimal('1609.344'))
+U.nautical_mile = DerivedUnit(primary_unit=U.m, ratio=Decimal('1852'))
 
 U.area = U.length**2
-U.hectare = DerivedUnit(primary_unit=U.m**2, ratio=10000.0)
-U.acre = DerivedUnit(primary_unit=U.m**2, ratio=4046.8564224)
+U.hectare = DerivedUnit(primary_unit=U.m**2, ratio=Decimal('10000'))
+U.acre = DerivedUnit(primary_unit=U.m**2, ratio=Decimal('4046.8564224'))
 
 U.volume = U.length**3
 U.l = DerivedUnit(primary_unit=U.m**3, ratio=Decimal('0.001'))
@@ -30,10 +30,10 @@ U.bbl = DerivedUnit(primary_unit=U.gallon, ratio=42.0)
 
 U.weight = PrimaryDimension()
 U.kg = PrimaryUnit(dimension=U.weight)
-U.g = DerivedUnit(primary_unit=U.kg, ratio=0.001, prefixes=('k', 'm'))
-U.mt = 1000 * U.kg
-U.pound = DerivedUnit(primary_unit=U.kg, ratio=0.45359237)
-U.toz = DerivedUnit(primary_unit=U.kg, ratio=1.0/32.1507)
+U.g = DerivedUnit(primary_unit=U.kg, ratio=Decimal('0.001'), prefixes=('k', 'm'))
+U.mt = Decimal('1000') * U.kg
+U.pound = DerivedUnit(primary_unit=U.kg, ratio=Decimal('0.45359237'))
+U.toz = DerivedUnit(primary_unit=U.kg, ratio=Decimal(1)/Decimal('32.1507'))
 
 U.time = PrimaryDimension()
 absolute_seconds = PrimaryUnit(dimension=U.time)
@@ -41,10 +41,10 @@ U.seconds_since_epoch = OffsetDerivedUnit(primary_unit=absolute_seconds)
 U.s = U.seconds_since_epoch.diff
 U.add_prefixes(U.s, ('m', 'u', 'n'))
 
-U.min = 60.0 * U.s
-U.h = 60.0 * U.min
-U.day = 24.0 * U.h
-U.week = 7.0 * U.day
+U.min = Decimal('60') * U.s
+U.h = Decimal('60') * U.min
+U.day = Decimal('24') * U.h
+U.week = Decimal('7') * U.day
 
 U.velocity = U.length / U.time
 U.kph = U.km / U.h
@@ -52,13 +52,12 @@ U.mph = U.mi / U.h
 
 U.temperature = PrimaryDimension()
 U.K = PrimaryUnit(dimension=U.temperature)
-U.degC = OffsetDerivedUnit(primary_unit=U.K, ratio=1.0, offset=273.15)
-U.degF = OffsetDerivedUnit(primary_unit=U.K, ratio=5.0/9.0, offset=459.67)
+U.degC = OffsetDerivedUnit(primary_unit=U.K, ratio=Decimal('1'), offset=Decimal('273.15'))
+U.degF = OffsetDerivedUnit(primary_unit=U.K, ratio=Decimal('5')/Decimal('9'), offset=Decimal('459.67'))
 
 U.energy = U.weight * U.length**2 / U.time**2
 U.J = PrimaryUnit(dimension=U.energy)
-U.cal = DerivedUnit(primary_unit=U.J, ratio=4.184)
-U.MMBtu = DerivedUnit(primary_unit=U.J, ratio=1055.05585262)
+U.cal = DerivedUnit(primary_unit=U.J, ratio=Decimal('4.184'))
 
 U.power = U.energy / U.time
 U.W = PrimaryUnit(dimension=U.power, prefixes=('k', 'M', 'G', 'T', 'm'))
