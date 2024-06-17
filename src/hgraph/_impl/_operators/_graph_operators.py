b77fde7e205b11408ed0f6e6d165e3a116ff7e32
Howard Henson <howard@henson.me.uk>
2024-06-17 20:50:30 +0400

    Move print and log operators and convert to proper graph

--- src/hgraph/_impl/_operators/_graph_operators.py
+++ src/hgraph/_impl/_operators/_graph_operators.py
@@ -1,4 +1,13 @@
-from hgraph._operators._graph_operators import default, nothing, null_sink
+import logging
+import sys
+from dataclasses import dataclass
+
+from hgraph._types._tsb_type import TSB, TS_SCHEMA
+from hgraph._operators._graph_operators import default, nothing, null_sink, debug_print, print_, log_
+from hgraph._operators._string import format_
+from hgraph._runtime._evaluation_clock import EvaluationClock
+from hgraph._types._scalar_types import CompoundScalar, STATE, LOGGER
+from hgraph._types._ts_type import TS
 from hgraph._types._ref_type import REF
 from hgraph._types._time_series_types import OUT, TIME_SERIES_TYPE
 from hgraph._types._type_meta_data import AUTO_RESOLVE
@@ -47,3 +56,89 @@ def null_sink_impl(ts: TIME_SERIES_TYPE):
     A sink node that will consume the time-series and do nothing with it.
     This is useful when you want to consume a time-series but do not want to do anything with it.
     """
+
+
+@dataclass
+class _DebugPrintState(CompoundScalar):
+    count: int = 0
+
+
+@sink_node(overloads=debug_print)
+def debug_print_impl(
+    label: str,
+    ts: TIME_SERIES_TYPE,
+    print_delta: bool = True,
+    sample: int = -1,
+    _clock: EvaluationClock = None,
+    _state: STATE[_DebugPrintState] = None,
+):
+    """
+    Use this to help debug code, this will print the value of the supplied time-series to the standard out.
+    It will include the engine time in the print. Do not leave these lines in production code.
+
+    :param label: The label to print before the value
+    :param ts: The time-series to print
+    :param print_delta: If true, print the delta value, otherwise print the value
+    :param sample: Only print an output for every sample number of ticks.
+    :param _clock: The evaluation clock (to be injected)
+    """
+    _state.count += 1
+    if sample < 2 or _state.count % sample == 0:
+        if print_delta:
+            value = ts.delta_value
+        else:
+            value = ts.value
+        if sample > 1:
+            count = f"[{_state.count}]"
+        else:
+            count = ""
+        print(f"[{_clock.now}][{_clock.evaluation_time}]{count} {label}: {value}")
+
+
+@graph(overloads=print_)
+def print_impl(format_str: TS[str], *args: TIME_SERIES_TYPE, __std_out__: bool = True, **kwargs: TSB[TS_SCHEMA]):
+    """
+    A sink node that will write the formatted string to the std out.
+    This should be generally be used for debugging purposes and not be present in production code, instead use the
+    log nodes for writing in a production context.
+
+    :param format_str: The format string as defined in format
+    :param args: The time-series enumerated inputs
+    :param kwargs: The named time-series inputs
+    """
+    if len(args) == 0 and len(kwargs) == 0:
+        return _print(format_str)
+    else:
+        return _print(format_(format_str, *args, **kwargs), std_out=__std_out__)
+
+
+@sink_node
+def _print(ts: TS[str], std_out: bool = True):
+    """
+    A sink node that will write the string time-series to the standard out.
+
+    :param ts: The string to write to the std out.
+    :param std_out: If true, print to std out else std err.
+    """
+    print(ts.value) if std_out else print(ts.value, file=sys.stderr)
+
+
+@graph(overloads=log_)
+def log_impl(format_str: TS[str], *args: TIME_SERIES_TYPE, level: int = logging.INFO, **kwargs: TSB[TS_SCHEMA]):
+    """
+    A sink node that will log the formatted string to the system logger.
+
+    :param format_str: The format string as defined in format
+    :param level: The logging level
+    :param args: The time-series enumerated inputs
+    :param kwargs: The named time-series inputs
+    """
+    if len(args) == 0 and len(kwargs) == 0:
+        return _log(format_str, level)
+    else:
+        return _log(format_(format_str, *args, **kwargs), level)
+
+
+@sink_node
+def _log(ts: TS[str], level: int, logger: LOGGER = None):
+    logger.log(level, ts.value)
--- src/hgraph/_operators/_graph_operators.py
+++ src/hgraph/_operators/_graph_operators.py
@@ -1,10 +1,14 @@
+import logging
+
 from hgraph._types._scalar_types import DEFAULT
+from hgraph._types._ts_type import TS
+from hgraph._types._tsb_type import TSB, TS_SCHEMA
 from hgraph._types._time_series_types import OUT, TIME_SERIES_TYPE
 from hgraph._types._type_meta_data import AUTO_RESOLVE
 from hgraph._wiring._decorators import operator
 
 
-__all__ = ("default", "nothing", "null_sink")
+__all__ = ("default", "nothing", "null_sink", "debug_print", "print_")
 
 
 @operator
@@ -55,4 +59,47 @@ def null_sink(ts: TIME_SERIES_TYPE):
     """
     A sink node that will consume the time-series and do nothing with it.
     This is useful when you want to consume a time-series but do not want to do anything with it.
+    """
+
+
+@operator
+def debug_print(
+    label: str,
+    ts: TIME_SERIES_TYPE,
+    print_delta: bool = True,
+    sample: int = -1
+):
+    """
+    Use this to help debug code, this will print the value of the supplied time-series to the standard out.
+    It will include the engine time in the print. Do not leave these lines in production code.
+
+    :param label: The label to print before the value
+    :param ts: The time-series to print
+    :param print_delta: If true, print the delta value, otherwise print the value
+    :param sample: Only print an output for every sample number of ticks.
+    """
+
+
+@operator
+def print_(format_str: TS[str], *args: TIME_SERIES_TYPE, __std_out__: bool = True, **kwargs:TSB[TS_SCHEMA]):
+    """
+    A sink node that will write the formatted string to the std out.
+    This should be generally be used for debugging purposes and not be present in production code, instead use the
+    log nodes for writing in a production context.
+
+    :param format_str: The format string as defined in format
+    :param args: The time-series enumerated inputs
+    :param kwargs: The named time-series inputs
+    """
+
+
+@operator
+def log_(format_str: TS[str], *args: TIME_SERIES_TYPE, level: int = logging.INFO, **kwargs: TSB[TS_SCHEMA]):
+    """
+    A sink node that will log the formatted string to the system logger.
+
+    :param format_str: The format string as defined in format
+    :param level: The logging level
+    :param args: The time-series enumerated inputs
+    :param kwargs: The named time-series inputs
     """
