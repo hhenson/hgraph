bc778ce95334b7fe2ccbf95c828b12832098aeeb
Simon Young <syoung@bamfunds.com>
2024-06-19 17:01:51 +0100

    no-op
    Signed-off-by: Simon Young <syoung@bamfunds.com>

--- src/hgraph/_impl/_operators/_date_time_operators.py
+++ src/hgraph/_impl/_operators/_date_time_operators.py
@@ -162,3 +162,4 @@ def var_datetime_unary(ts: TS[datetime]) -> TS[datetime]:
 @graph(overloads=var)
 def var_datetime_unary(ts: TS[datetime]) -> TS[datetime]:
     raise WiringError("Cannot calculate the variance of dates")
+
