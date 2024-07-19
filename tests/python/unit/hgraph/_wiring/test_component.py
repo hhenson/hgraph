3d0028dedad5bef8fc1a1d1bc865c2cb58886fcf
Howard Henson <howard@henson.me.uk>
2024-07-19 13:08:42 +0400

    Update component to track and build recordable ids

--- tests/python/unit/hgraph/_wiring/test_component.py
+++ tests/python/unit/hgraph/_wiring/test_component.py
@@ -1,4 +1,6 @@
-from hgraph import component, TS
+import pytest
+
+from hgraph import component, TS, graph
 from hgraph.test import eval_node
 
 
@@ -9,3 +11,27 @@ def test_component():
         return ts + 1.0
 
     assert eval_node(my_component, ts=[1.0, 2.0, 3.0], key="key_1") == [2.0, 3.0, 4.0]
+
+
+def test_component_error_duplicate_id():
+    @component
+    def my_component(ts: TS[float], key: str) -> TS[float]:
+        return ts + 1.0
+
+    @graph
+    def duplicate_wiring(ts: TS[float], key: str) -> TS[float]:
+        a = my_component(ts=ts, key=key)
+        b = my_component(ts=ts + 1, key=key)
+        return a + b
+
+    with pytest.raises(RuntimeError):
+        assert eval_node(duplicate_wiring, ts=[1.0, 2.0, 3.0], key="key_1") == [2.0, 3.0, 4.0]
+
+
+def test_recordable_id_from_ts():
+
+    @component(recordable_id="Test_{key}")
+    def my_component(ts: TS[float], key: TS[str]) -> TS[float]:
+        return ts + 1.0
+
+    assert eval_node(my_component, ts=[1.0, 2.0, 3.0], key=["key_1"]) == [2.0, 3.0, 4.0]
