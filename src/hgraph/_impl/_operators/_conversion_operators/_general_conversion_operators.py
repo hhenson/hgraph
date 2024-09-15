22a03ae97e0db971f8d227b0c5b03674f28f2b76
Howard Henson <howard@henson.me.uk>
2024-09-15 15:19:32 +0400

    Add requires check to correctly handle the match of covert_noop

--- src/hgraph/_impl/_operators/_conversion_operators/_general_conversion_operators.py
+++ src/hgraph/_impl/_operators/_conversion_operators/_general_conversion_operators.py
@@ -1,12 +1,16 @@
 from typing import Type
 
-from hgraph import OUT, DEFAULT, graph, convert
+from hgraph import OUT, DEFAULT, graph, convert, TIME_SERIES_TYPE
 
 __all__ = ()
 
 
-@graph(overloads=convert)
-def convert_noop(ts: OUT, to: Type[OUT] = OUT) -> DEFAULT[OUT]:
+@graph(
+    overloads=convert,
+    requires=lambda m, s: m[TIME_SERIES_TYPE] == m[OUT]
+    or f"OUT: {m[OUT]} is not the same as ts: {m[TIME_SERIES_TYPE]}",
+)
+def convert_noop(ts: TIME_SERIES_TYPE, to: Type[OUT] = OUT) -> DEFAULT[OUT]:
     """
     if types are the same, then return the value.
     """
