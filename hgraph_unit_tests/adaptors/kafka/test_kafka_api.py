88f48121cc8a4f556b01b3bca41628ccf36e3d89
Howard Henson <howard@henson.me.uk>
2025-04-24 20:21:30 +0400

    Make publishing work in the most simplistic mode.

--- hgraph_unit_tests/adaptors/kafka/test_kafka_api.py
+++ hgraph_unit_tests/adaptors/kafka/test_kafka_api.py
@@ -1,15 +1,47 @@
-from hgraph import graph, TS, debug_print, const
+from unittest.mock import MagicMock, patch, PropertyMock
+
+import pytest
+
+from hgraph import graph, TS, debug_print, const, GlobalState
 from hgraph.adaptors.kafka import register_kafka_adaptor, message_subscriber, message_publisher
 from hgraph.test import eval_node
 
 
+@pytest.fixture
+def mock_kafka_producer():
+    # Create a mock producer instance
+    producer = MagicMock()
+    # Mock the asynchronous 'send' method to return a mock Future
+    mock_future = MagicMock()
+    mock_future.add_callback.return_value = None
+    mock_future.add_errback.return_value = None
+    producer.send.return_value = mock_future
+    producer.flush.return_value = None
+    producer.close.return_value = None
+    return producer
+
+
+@pytest.fixture
+def mock_kafka_state(mock_kafka_producer):
+    # Mock the KafkaClientsState instance
+    with GlobalState() as gs:
+        from hgraph.adaptors.kafka._impl import KafkaMessageState
+
+        state_instance = KafkaMessageState().instance()
+        # Just for the value of the _kafka_producer and up the count to make sure
+        # We don't try and kill it for now.
+        state_instance._kafka_producer = mock_kafka_producer
+        state_instance._kafka_producer_count += 1
+        yield state_instance
+
+
 def test_subscriber():
 
-    @message_subscriber(topic="my_topic")
+    @message_subscriber(topic="test")
     def my_subscriber(msg: TS[bytes]):
         debug_print("test_subs1", msg)
 
-    @message_subscriber(topic="my_topic")
+    @message_subscriber(topic="test")
     def my_other_subscriber(msg: TS[bytes], recovered: TS[bool]):
         debug_print("test_subs2:msg", msg)
         debug_print("test_subs2:recovered", recovered)
@@ -23,11 +55,11 @@ def test_subscriber():
     assert eval_node(g) == None
 
 
-def test_publisher():
+def test_publisher(mock_kafka_state, mock_kafka_producer):
 
-    @message_publisher(topic="my_topic")
+    @message_publisher(topic="test")
     def my_publisher() -> TS[bytes]:
-        return const(b'my publisher')
+        return const(b"my publisher")
 
     @graph
     def g():
@@ -35,3 +67,6 @@ def test_publisher():
         my_publisher()
 
     assert eval_node(g) == None
+    assert mock_kafka_producer.send.call_count == 1
+    assert mock_kafka_producer.send.call_args[0][0] == "test"
+    assert mock_kafka_producer.send.call_args[0][1] == b"my publisher"
--- src/hgraph/adaptors/kafka/_impl.py
+++ src/hgraph/adaptors/kafka/_impl.py
@@ -1,35 +1,55 @@
 from dataclasses import dataclass, field
+from datetime import timedelta
 
-from hgraph import adaptor_impl, adaptor, WiringGraphContext, register_adaptor, GlobalState, register_service, TS, \
-    debug_print, const, service_impl, MIN_TD, TSB, combine, sink_node
-from hgraph.adaptors.kafka._api import message_publisher_operator, message_subscriber_service, \
-    message_history_subscriber_service, MessageState
+from kafka import KafkaConsumer, KafkaProducer, KafkaAdminClient
+
+from hgraph import (
+    adaptor_impl,
+    adaptor,
+    WiringGraphContext,
+    register_adaptor,
+    GlobalState,
+    register_service,
+    TS,
+    debug_print,
+    const,
+    service_impl,
+    MIN_TD,
+    TSB,
+    combine,
+    sink_node,
+    STATE,
+    EvaluationEngineApi,
+    SCHEDULER,
+)
+
+from hgraph.adaptors.kafka._api import (
+    message_publisher_operator,
+    message_subscriber_service,
+    message_history_subscriber_service,
+    MessageState,
+)
 
 __all__ = ("register_kafka_adaptor",)
 
 
-@dataclass
-class KafkaConfig:
-    """Start with a few of the most basic configuration options"""
-    bootstrap_servers: tuple[str, ...]
-    buffer_memory: int = None
-    max_request_size: int = None
-
-
-def register_kafka_adaptor(config: KafkaConfig):
+def register_kafka_adaptor(config: dict):
     # At some point, we can use a path to put the service message state on, then this can be used to support
     # multiple messaging services concurrently.
-    KafkaMessageState.instance().config = config
+    (ms := KafkaMessageState.instance()).config = config
 
 
 @dataclass
 class KafkaMessageState(MessageState):
     """Tracks the registered topics and their replay state."""
+
     subscribers: set[str] = field(default_factory=set)
     history_subscribers: set[str] = field(default_factory=set)
     publishers: set[str] = field(default_factory=set)
+    _kafka_producer: KafkaProducer = None
+    _kafka_producer_count: int = 0
 
-    config: KafkaConfig = None
+    config: dict = None
 
     @classmethod
     def instance(cls) -> "KafkaMessageState":
@@ -54,6 +74,21 @@ class KafkaMessageState(MessageState):
             self.history_subscribers.add(topic)
             register_service(topic, _message_subscriber_history_aggregator, topic=topic)
 
+    @property
+    def producer(self) -> KafkaProducer:
+        if self._kafka_producer is None:
+            self._kafka_producer = KafkaProducer(**self.config)
+        self._kafka_producer_count += 1
+        return self._kafka_producer
+
+    def close_producer(self):
+        if self._kafka_producer is None:
+            raise ValueError("No producer to close")
+        self._kafka_producer_count -= 1
+        if self._kafka_producer_count == 0:
+            self._kafka_producer.close()
+            self._kafka_producer = None
+
 
 def _registered_topics(m, s):
     """
@@ -65,21 +100,41 @@ def _registered_topics(m, s):
     return topic in ms.publishers
 
 
-@sink_node(overloads=message_publisher_operator, requires=_registered_topics )
-def _message_publisher_aggregator(msg: TS[bytes], topic: str):
-    print(f"publish topic: {topic}", msg.value)
+@sink_node(overloads=message_publisher_operator, requires=_registered_topics)
+def _kafka_message_publisher(msg: TS[bytes], topic: str, _state: STATE = None, _scheduler: SCHEDULER = None) -> None:
+    if msg.modified:
+        _state.producer.send(topic, msg.value)
+        _scheduler.schedule(
+            timedelta(milliseconds=100), tag="flush_timer"
+        )  # This will re-schedule the flush timer if already set.
+
+    if _scheduler.is_scheduled_now:
+        # Make sure we flush reasonably regularly.
+        _state.producer.flush()
 
 
-@service_impl(interfaces=(message_subscriber_service,))
+@_kafka_message_publisher.start
+def _kafka_message_publisher_start(topic: str, _state: STATE):
+    _state.producer = KafkaMessageState.instance().producer
+
+
+@_kafka_message_publisher.stop
+def _kafka_message_publisher_stop(_state: STATE):
+    _state.producer.flush()
+    _state.producer = None
+    KafkaMessageState.instance().close_producer()
+
+
+@service_impl(interfaces=message_subscriber_service)
 def _message_subscriber_aggregator(path: str, topic: str) -> TS[bytes]:
     print(f"subscribe topic: {topic}")
-    topic_b = b'sub: ' + topic.encode("utf-8")
-    return const(topic_b, delay=MIN_TD*2)
+    topic_b = b"sub: " + topic.encode("utf-8")
+    return const(topic_b, delay=MIN_TD * 2)
 
 
 @service_impl(interfaces=(message_history_subscriber_service,))
-def _message_subscriber_history_aggregator(path: str, topic: str) -> TSB["msg": TS[bytes], "recovered": TS[bool]]:
+def _message_subscriber_history_aggregator(path: str, topic: str) -> TSB["msg" : TS[bytes], "recovered" : TS[bool]]:
     """Recovered must tick after the last message has been delivered."""
     print(f"subscribe topic: {topic}")
-    topic_b = b'sub_h: ' + topic.encode("utf-8")
+    topic_b = b"sub_h: " + topic.encode("utf-8")
     return combine[TSB](msg=const(topic_b), recovered=const(True, delay=MIN_TD))
