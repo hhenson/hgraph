be6d66a41ff0b699d3cae42b038bd61f5fab3ab4
Howard Henson <howard@henson.me.uk>
2025-05-06 17:25:23 +0400

    Add call method to call a flat function

--- src/hgraph/_operators/_apply.py
+++ src/hgraph/_operators/_apply.py
@@ -1,9 +1,10 @@
 from typing import Callable
 
+from hgraph import sink_node
 from hgraph._types import DEFAULT, TSB, TS_SCHEMA, TS_SCHEMA_1, TS, OUT, TIME_SERIES_TYPE
 from hgraph._wiring._decorators import compute_node
 
-__all__ = ["apply"]
+__all__ = ["apply", "call"]
 
 
 @compute_node(valid=("fn",), resolvers={TIME_SERIES_TYPE: lambda m, s: TS[s['fn'].__annotations__['return']]})
@@ -23,3 +24,21 @@ def apply(fn: TS[Callable], *args: TSB[TS_SCHEMA],
         **({k: v.value for k, v in kwargs.items() if v.valid} if kwargs else {}),
     )
     return out
+
+
+@sink_node(valid=("fn",))
+def call(fn: TS[Callable], *args: TSB[TS_SCHEMA],
+          **kwargs: TSB[TS_SCHEMA_1]):
+    """
+    Apply the inputs to the fn provided.
+    This allows a function to be passed as a time-series value, and it will be used to evaluate the inputs as they tick.
+    This will not allow the function to be called unless are args are valid, but this is not required for kwargs.
+    """
+    if not all(a.valid for a in args):
+        # Don't process unless all args are valid.
+        return
+    fn_ = fn.value
+    fn_(
+        *((a.value for a in args.values()) if args else tuple()),
+        **({k: v.value for k, v in kwargs.items() if v.valid} if kwargs else {}),
+    )
