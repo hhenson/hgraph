bb2bbcd1e6eaa0db2e27b4ef0a2b055cdfa82fa0
Howard Henson <howard@henson.me.uk>
2025-05-22 11:04:12 +0400

    Add the ability to set a break-point in a graph.

--- src/hgraph/test/__init__.py
+++ src/hgraph/test/__init__.py
@@ -1,3 +1,4 @@
+from hgraph.test._breakpoint import *
 from hgraph.test._node_printer import *
 from hgraph.test._node_unit_tester import *
 from hgraph.test._node_profiler import *
---
+++ src/hgraph/test/_breakpoint.py
@@ -0,0 +1,67 @@
+"""
+Ability to create breakpoints in the graph.
+"""
+from hgraph import compute_node, TSB, TS_SCHEMA, graph, const, debug_print, operator, OUT, TS
+from hgraph.test import eval_node
+
+
+__all__ = ("breakpoint_",)
+
+
+@operator
+def breakpoint_(ts: OUT) -> OUT:
+    """
+    Place a breakpoint in the graph.
+    There are three key types of breakpoints supported:
+
+    * breakpoint_(ts) - Breaks when the ts value is modified.
+    * breakpoint_(condition, ts) - Breaks when the condition is True and either the condition or the value is modified.
+    * breakpoint_(**kwargs) - Breaks when any of the inputs are modified.
+    """
+
+
+@compute_node(overloads=breakpoint_)
+def breakpoint_ts(ts: OUT) -> OUT:
+    breakpoint()
+    return ts.delta_value
+
+
+@compute_node(overloads=breakpoint_, valid=("ts",))
+def breakpoint_conditional(condition: TS[bool], ts: OUT) -> OUT:
+    if condition.valid and condition.value:
+        breakpoint()
+    return ts.delta_value
+
+
+@compute_node(overloads=breakpoint_, valid=tuple())
+def breakpoint_many(**kwargs: TSB[TS_SCHEMA]) -> TSB[TS_SCHEMA]:
+    breakpoint()
+    return kwargs.delta_value
+
+
+if __name__ == "__main__":
+    @graph
+    def breakpoint_graph():
+        c = const(1)
+        debug_print("value", breakpoint_(c))
+
+
+    eval_node(breakpoint_graph)
+
+
+    @graph
+    def breakpoint_conditional_graph(condition: TS[bool], value: TS[int]):
+        debug_print("value", breakpoint_(condition, value))
+
+
+    eval_node(breakpoint_conditional_graph, [None, False, True], value=[1, 2, 3])
+
+
+    @graph
+    def breakpoint_many_graph(a: TS[int], b: TS[int], c: TS[int]):
+        a, b, c = breakpoint_many(a=a, b=b, c=c)
+        debug_print("a", a)
+        debug_print("b", b)
+        debug_print("c", c)
+
+    eval_node(breakpoint_many_graph, [1, None, 3], [None, 4, None], [None, None, 6])
