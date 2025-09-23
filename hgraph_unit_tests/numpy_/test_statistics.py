2e3297e1b7ac6bb990f2fd2387d327295e64a382
Howard Henson <howard@henson.me.uk>
2025-09-23 04:28:59 +0000

    Add quantile operator

--- hgraph_unit_tests/numpy_/test_statistics.py
+++ hgraph_unit_tests/numpy_/test_statistics.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from hgraph import TS, Array, Size, graph
+from hgraph import TS, Array, Size, graph, WindowSize, to_window
 from hgraph.numpy_ import corrcoef
 from hgraph.test import eval_node
 
@@ -34,3 +34,24 @@ def test_corrcoef_3():
     actual = eval_node(g, [np.array([1, 2, 3, 4])])
     assert (actual[0] == np.array([[1.0, 1.0], [1.0, 1.0]])).all()
     
+def test_quantile_1():
+
+    from hgraph.numpy_ import quantile
+
+    @graph
+    def g(ts: TS[Array[int, Size[4]]]) -> TS[float]:
+        return quantile(ts, 0.5)
+
+    actual = eval_node(g, [np.array([1, 2, 3, 4])])
+    assert actual == [2.5]
+
+def test_quantile_2():
+
+    from hgraph.numpy_ import quantile
+
+    @graph
+    def g(ts: TS[int]) -> TS[float]:
+        return quantile(to_window(ts, 4), .5)
+
+    actual = eval_node(g, [1, 2, 3, 4])
+    assert actual == [None, None, None, 2.5]
--- src/hgraph/numpy_/_statistics.py
+++ src/hgraph/numpy_/_statistics.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-from hgraph import TS, graph, AUTO_RESOLVE, Array, Size, compute_node, SCALAR
+from hgraph import TS, graph, AUTO_RESOLVE, Array, Size, compute_node, SCALAR, NUMBER, operator
+from hgraph import TSW, WINDOW_SIZE, WINDOW_SIZE_MIN, OUT, SIZE, TIME_SERIES_TYPE
 from hgraph.numpy_._constants import ARRAY
 from hgraph.numpy_._utils import extract_dimensions_from_array, extract_type_from_array, add_docs
 
 __all__ = [
-    "corrcoef",
+    "corrcoef", "quantile"
 ]
 
 
@@ -40,3 +41,39 @@ def _corrcoef_no_y(x: TS[ARRAY], rowvar: bool) -> TS[SCALAR]:
 @compute_node
 def _corrcoef(x: TS[ARRAY], y: TS[ARRAY], rowvar: bool) -> TS[SCALAR]:
     return np.corrcoef(x.value, y.value, rowvar=rowvar)
+
+
+@operator
+@add_docs(np.quantile)
+def quantile(
+    a: TIME_SERIES_TYPE,
+    q: TS[SCALAR],
+    method: str = "linear",
+    keepdims: bool = False,
+) -> OUT:
+    """
+    This will wrap most common usages of np.quantile, but not all. The base case of array and single float q is supported.
+    """
+    
+
+@compute_node(overloads=quantile)
+def quantile_array_scalar(
+    a: TS[Array[NUMBER, SIZE]],
+    q: TS[float],
+    method: str = "linear",
+    keepdims: bool = False,
+) -> TS[float]:
+    v = np.quantile(a.value, q.value, method=method, keepdims=keepdims)
+    return float(v)
+
+
+@compute_node(overloads=quantile, all_valid=("a",))
+def quantile_tsw_scalar(
+    a: TSW[NUMBER, WINDOW_SIZE, WINDOW_SIZE_MIN],
+    q: TS[float],
+    method: str = "linear",
+    keepdims: bool = False,
+) -> TS[float]:
+    v = np.quantile(a.value, q.value, method=method, keepdims=keepdims)
+    return float(v)
+
