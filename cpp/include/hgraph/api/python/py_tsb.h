8f7a41d5689c53ee01f5fcffd5ae157359d9147d
Howard Henson <howard@henson.me.uk>
2025-11-18 17:04:47 +0400

    In process of converting more types to use wrapped interfaces.

---
+++ cpp/include/hgraph/api/python/py_tsb.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <hgraph/api/python/py_time_series.h>
+#include <hgraph/hgraph_base.h>
+
+namespace hgraph
+{
+
+    template <typename T_TS, typename T_U>
+    concept is_py_tsb = ((std::is_same_v<T_TS, PyTimeSeriesInput> || std::is_same_v<T_TS, PyTimeSeriesOutput>) &&
+                         ((std::is_same_v<T_TS, PyTimeSeriesInput> && std::is_same_v<T_U, TimeSeriesBundleInput>) ||
+                          (std::is_same_v<T_TS, PyTimeSeriesOutput> && std::is_same_v<T_U, TimeSeriesBundleOutput>)));
+
+    template <typename T_TS, typename T_U>
+        requires(is_py_tsb<T_TS, T_U>)
+    struct PyTimeSeriesBundle : T_TS
+    {
+        using underlying_type = T_U;
+
+        // Default iterator iterates over keys to keep this more consistent with Python (c.f. dict)
+        [[nodiscard]] nb::object iter() const;
+
+        [[nodiscard]] nb::object get_item(const nb::handle &key) const;
+
+        [[nodiscard]] nb::bool_ contains(const nb::handle &key) const;
+
+        [[nodiscard]] const TimeSeriesSchema &schema() const;
+
+        // Retrieves valid keys
+        [[nodiscard]] nb::object keys() const;
+
+        [[nodiscard]] nb::object valid_keys() const;
+
+        [[nodiscard]] nb::object modified_keys() const;
+
+        [[nodiscard]] nb::int_ len() const;
+
+        // Retrieves valid items
+        [[nodiscard]] nb::object items() const;
+
+        [[nodiscard]] nb::object valid_items() const;
+
+        [[nodiscard]] nb::object modified_items() const;
+
+      private:
+        underlying_type *impl() const;
+    };
+
+
+    using PyTimeSeriesBundleOutput = PyTimeSeriesBundle<PyTimeSeriesOutput, TimeSeriesBundleOutput>;
+
+    using PyTimeSeriesBundleInput = PyTimeSeriesBundle<PyTimeSeriesInput, TimeSeriesBundleInput>;
+
+    void register_tsb_with_nanobind(nb::module_ &m);
+
+}  // namespace hgraph
