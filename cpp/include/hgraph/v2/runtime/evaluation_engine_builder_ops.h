635f720850a9b95ff11b4c69e9a578f27253c93e
Howard Henson <howard@henson.me.uk>
2026-04-20 11:25:37 +0400

    Some outlines for code structure

---
+++ cpp/include/hgraph/v2/runtime/evaluation_engine_builder_ops.h
@@ -0,0 +1,13 @@
+//
+// Created by Howard Henson on 20/04/2026.
+//
+
+#ifndef HGRAPH_CPP_ROOT_EVALUATION_ENGINE_BUILDER_OPS_H
+#define HGRAPH_CPP_ROOT_EVALUATION_ENGINE_BUILDER_OPS_H
+
+namespace hgraph
+{
+    struct EvaluationEngineBuilderOps {};
+}
+
+#endif //HGRAPH_CPP_ROOT_EVALUATION_ENGINE_BUILDER_OPS_H
---
+++ cpp/include/hgraph/v2/types/graph/graph_builder.h
@@ -0,0 +1,13 @@
+//
+// Created by Howard Henson on 20/04/2026.
+//
+
+#ifndef HGRAPH_CPP_ROOT_GRAPH_BUILDER_H
+#define HGRAPH_CPP_ROOT_GRAPH_BUILDER_H
+
+namespace hgraph
+{
+    struct GraphBuilder {};
+}
+
+#endif //HGRAPH_CPP_ROOT_GRAPH_BUILDER_H
