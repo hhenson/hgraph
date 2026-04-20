5551c467fca5ce7a3d0b19a5846ed1b608ae1f6c
Howard Henson <howard@henson.me.uk>
2026-04-20 11:44:31 +0400

    Some outlines for code structure

---
+++ cpp/include/hgraph/v2/types/metadata/value_type_meta_data.h
@@ -0,0 +1,13 @@
+//
+// Created by Howard Henson on 20/04/2026.
+//
+
+#ifndef HGRAPH_CPP_ROOT_VALUE_TYPE_META_DATA_H
+#define HGRAPH_CPP_ROOT_VALUE_TYPE_META_DATA_H
+
+namespace hgraph
+{
+
+}
+
+#endif //HGRAPH_CPP_ROOT_VALUE_TYPE_META_DATA_H
