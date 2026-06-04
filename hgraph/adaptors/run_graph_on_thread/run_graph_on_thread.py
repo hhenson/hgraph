f2252720c976fbefcdae07e4c9f50d1d7836cb59
Simon Young <syoung@bamfunds.com>
2026-06-04 08:42:09 +0100

    add delta arg to publish_output
    Signed-off-by: Simon Young <syoung@bamfunds.com>

--- hgraph/adaptors/run_graph_on_thread/run_graph_on_thread.py
+++ hgraph/adaptors/run_graph_on_thread/run_graph_on_thread.py
@@ -37,8 +37,8 @@ class RunGraphOutput(TimeSeriesSchema, Generic[OUT]):
 
 
 @sink_node
-def publish_output(ts: TIME_SERIES_TYPE, delta: bool = True, _global_state: GlobalState = None):
-    _global_state.output_queue(ts.delta_value)
+def publish_output(ts: TIME_SERIES_TYPE, delta: bool = True):
+    GlobalState.instance().output_queue(ts.delta_value if delta else ts.value)
 
 
 @service_adaptor
