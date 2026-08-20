77f5d21b9ae076754761e3d7aa3932a2cbbc51a8
Howard Henson <howard@henson.me.uk>
2026-08-20 02:35:25 +0100

    Update fabric audit fixture

--- extensions/fabric/python/tests/test_distribution_audit.py
+++ extensions/fabric/python/tests/test_distribution_audit.py
@@ -27,6 +27,7 @@ def test_wheel_audit_accepts_platform_library_directories(
         "include/hgraph/fabric/metadata_codec.h": "",
         "include/hgraph/fabric/notifier.h": "",
         "include/hgraph/fabric/operators.h": "",
+        "include/hgraph/fabric/planning.h": "",
         "include/hgraph/fabric/publication.h": "",
         "include/hgraph/fabric/types.h": "",
         "include/hgraph/fabric/value_builders.h": "",
