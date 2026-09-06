1a2eac03a14d2853407579c73ce097f469ae0ac5
Howard Henson <howard@henson.me.uk>
2026-09-06 04:10:44 +0400

    test(language): guard every backend source

--- language/tests/cmake/backend_architecture.cmake
+++ language/tests/cmake/backend_architecture.cmake
@@ -2,16 +2,11 @@ if(NOT DEFINED HGL_LANGUAGE_SOURCE_DIR)
     message(FATAL_ERROR "HGL_LANGUAGE_SOURCE_DIR is required")
 endif()
 
-file(GLOB_RECURSE _codegen_sources LIST_DIRECTORIES FALSE
+file(GLOB_RECURSE _backend_sources LIST_DIRECTORIES FALSE
     "${HGL_LANGUAGE_SOURCE_DIR}/codegen/*.h"
     "${HGL_LANGUAGE_SOURCE_DIR}/codegen/*.cpp"
-)
-set(_backend_sources
-    ${_codegen_sources}
-    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/backend.h"
-    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/backend.cpp"
-    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/type_bridge.h"
-    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/type_bridge.cpp"
+    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/*.h"
+    "${HGL_LANGUAGE_SOURCE_DIR}/wiring/*.cpp"
 )
 
 foreach(_source IN LISTS _backend_sources)
